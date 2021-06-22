2d9dad5912894152fb44e468f6f6642c9eac705a
khdscor <khds_cor@naver.com>
2021-06-22 18:21:21 +0900

    feat: 전화번호 목록(프로그래머스 해시문제) 2차(91.7점)

--- src/hash/practice2/Solution.java
+++ src/hash/practice2/Solution.java
@@ -3,6 +3,7 @@ class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
         String compare="";
+
         exit:for(int length=2;length<5;length++) {
 
             for (String i : phone_book) {
@@ -12,7 +13,8 @@ class Solution {
                     for (String j : phone_book) {
                         if (i == j)
                             continue;
-                        if (j.contains(i)) {
+                        if (j.contains(i) && j.indexOf(i)==0) {
+
                             answer = false;
                             break exit;
                         }
